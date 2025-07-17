420c8e831c1278c0a3613063df404a6fc2413b6d
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2025-07-17 22:38:51 +0530

    Removed unnecessary request and object objects.

--- Database/config.js
+++ Database/config.js
@@ -3,14 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const connectDB = async (req,res) => {
+const connectDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGODB_URL);
     console.log("MonogoDB Connected Successfully.");
     return connection;
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "MongoDB Connection Error." });
+    console.log("MongoDB Connection Error: ",error.message);
   }
 };
 
